e292da67b554b6e30fed686089af40a5f38d2b81
iaingturner <80290838+iaingturner@users.noreply.github.com>
2024-06-19 10:31:41 +0100

    .

--- src/scripts/gpt.py
+++ src/scripts/gpt.py
@@ -1,5 +1,6 @@
 import openai
 import logging
+import os
 from datetime import datetime
 import time
 
@@ -15,16 +16,17 @@ class QueryGPT():
         openai.api_key = self.__open_ai_api_key
         
         # Create log file
+        os.makedirs('logs', exist_ok=True)
         logging.basicConfig(filename=f'logs/{datetime.now().strftime("%Y%m%d%H%M%S")}_chatgpt.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
         try:
             
-            response = openai.ChatCompletion.create(model=self.__model, 
-                                                    messages=[{"role":"user","content":search_string}], temperature=0.7)
+            response = openai.completions.create(model=self.__model, 
+                                                    prompt=[{"role":"user","content":search_string}], temperature=0.7)
             logging.info(f"Request successful") 
             return response
 
-        except openai.error.RateLimitError as e:
+        except openai.RateLimitError as e:
             
             retry_time = e.retry_after if hasattr(e, 'retry_after') else 30
             logging.info(f"Rate limit exceeded. Retrying in {retry_time} seconds...")
@@ -32,14 +34,7 @@ class QueryGPT():
             time.sleep(retry_time)
             return
 
-        except openai.error.ServiceUnavailableError as e:
-            retry_time = 10  # Adjust the retry time as needed
-            logging.info(f"Service is unavailable. Retrying in {retry_time} seconds...")
-            print(f"Service is unavailable. Retrying in {retry_time} seconds...")
-            time.sleep(retry_time)
-            return
-
-        except openai.error.APIError as e:
+        except openai.APIError as e:
             retry_time = e.retry_after if hasattr(e, 'retry_after') else 30
             logging.info(f"API error occurred. Retrying in {retry_time} seconds...")
             print(f"API error occurred. Retrying in {retry_time} seconds...")
